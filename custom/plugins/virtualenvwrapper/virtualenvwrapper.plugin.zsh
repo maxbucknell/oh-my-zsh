d50626a83c78e6e15ccf09dcee4e249eaf160155
Max Bucknell <me@maxbucknell.com>
2013-04-19 23:06:02 +0100

    Add autoenv script

--- custom/plugins/virtualenvwrapper/virtualenvwrapper.plugin.zsh
+++ custom/plugins/virtualenvwrapper/virtualenvwrapper.plugin.zsh
@@ -49,6 +49,25 @@ if [[ -f "$wrapsource" ]]; then
         builtin cd "$@" && workon_cwd
     }
   fi
+  
+  # The use_env call below is a reusable command to activate/create a new Python
+  # virtualenv, requiring only a single declarative line of code in your .env files.
+  # It only performs an action if the requested virtualenv is not the current one.
+  use_env() {
+    typeset venv
+    venv="$1"
+    if [[ "${VIRTUAL_ENV:t}" != "$venv" ]]; then
+      if workon | grep -q "$venv"; then
+        workon "$venv"
+      else
+        echo -n "Create virtualenv $venv now? (Yn) "
+        read answer
+        if [[ "$answer" == "Y" ]]; then
+          mkvirtualenv "$venv"
+        fi
+      fi
+    fi
+  }
 else
   print "zsh virtualenvwrapper plugin: Cannot find virtualenvwrapper_lazy.sh. Please install with \`pip install virtualenvwrapper\`."
 fi
